3c0b3ad856bd0d4f1783a289a7ecc3d23d8b9d15
Eric Garza <garzparz@gmail.com>
2018-03-19 16:08:20 -0700

    Single Server example

--- main.tf
+++ main.tf
@@ -3,10 +3,32 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami		= "ami-d874e0a0"
+  ami		= "ami-79873901"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
 
   tags {
-    Name = "terrform-exmample"
+    Name = "terraform-example"
   }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value = "${aws_instance.example.public_ip}"
 }
